a87574edb376e6f2baa401310286a082911c3932
faith wangari njau <faithjonnes91@gmail.com>
2024-10-08 08:53:49 +0300

    add text

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce(function (total, element) {
+    return total + element; // Correctly sum the total and element
+}, 0); // Initialize the accumulator to 0
+
+console.log(totalBatteries); // Outputs: 31
